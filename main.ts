ff6c9c6da969bb4262d0b07682953f2f93b05b67
VictoriaSandovalToledo <110991718+VictoriaSandovalToledo@users.noreply.github.com>
2022-09-08 07:46:14 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let numero1 = 0
+let numero2 = 0
+input.onButtonPressed(Button.A, function () {
+    numero1 = randint(0, 10)
+    basic.showNumber(numero1)
+    basic.showString("+")
+    numero2 = randint(0, 10)
+    basic.showNumber(numero2)
+    basic.showString("=")
+    basic.showNumber(numero1 + numero2)
+})
+input.onButtonPressed(Button.B, function () {
+    numero1 = randint(0, 10)
+    basic.showNumber(numero1)
+    basic.showString("°C")
+    basic.showString("=")
+    basic.showNumber(numero1 * (1.8 + 32))
+    basic.showString("°F")
+})
